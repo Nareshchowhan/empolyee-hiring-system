e07fdc89bda64b580c937a53367adbf82b9532b7
Naresh <nkorra481@gmail.com>
2022-07-02 14:37:54 +0530

    adding dotenv to server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+require('dotenv').config();
+const app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+app.listen(process.env.PORT,() => {
+    console.log('server started');
+});
+ 
